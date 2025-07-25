2c2a7f3bc98f04bffc69d64fd7785d8ee4a3ab0d
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2025-07-25 20:53:01 -0300

    FEAT: bot funcionando, agr e problema de postagem

--- src/index.js
+++ src/index.js
@@ -1,48 +1,64 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const cron = require('node-cron')
+const cron = require('node-cron');
 
-const { buscarPromocoesAmazon } = require('./scraper');
+
+const { buscarPromocoesAmazon } = require('./scraper.js');
+const { buscarPromocoesMercadoLivre } = require('./mecadoLivreScraper.js'); 
 const { gerarLinkAfiliado } = require('./afiliado');
 const { postarNoCanal } = require('./telegram');
 
-const AMAZON_AFILIATE_TAG = process.env.AMAZON_AFILIATE_TAG
-const TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID
+const AMAZON_AFFILIATE_TAG = process.env.AMAZON_AFFILIATE_TAG;
+const TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID;
 
-async function rodarBot(params) {
-    console.log('====================================')
-    console.log('iniciando nova verificacoes de ofertas ')
-
+async function rodarBot() {
+    console.log('====================================');
+    console.log('Iniciando nova verificação de ofertas...');
 
     try {
+        
+        console.log('Buscando promoções na Amazon...');
+        const produtosAmazon = await buscarPromocoesAmazon();
+        console.log(`-> Encontrados ${produtosAmazon.length} produtos na Amazon.`);
 
-        const produtos = await buscarPromocoesAmazon()
+        console.log('Buscando promoções no Mercado Livre...');
+        const produtosML = await buscarPromocoesMercadoLivre();
+        console.log(`-> Encontrados ${produtosML.length} produtos no Mercado Livre.`);
 
-        if (produtos.length === 0){
-            console.log (' nenhuma  promocao encontrada desta vez ')
-            return 
+        // 3. JUNTAMOS OS RESULTADOS EM UMA ÚNICA LISTA
+        const todosOsProdutos = [...produtosAmazon, ...produtosML];
+        console.log(`Total de ${todosOsProdutos.length} produtos encontrados nas duas lojas.`);
+
+        if (todosOsProdutos.length === 0) {
+            console.log('Nenhuma promoção encontrada desta vez.');
+            return;
         }
 
-        for ( const produto of produtos) {
-            const linkAfiliado = gerarLinkAfiliado(produto.link,AMAZON_AFILIATE_TAG)
-            await postarNoCanal(produto,linkAfiliado,TELEGRAM_CHANNEL_ID)
+        for (const produto of todosOsProdutos) {
+            let linkAfiliado = produto.link; 
 
-            await new Promise(resolve => setTimeout(resolve,5000))
+            if (produto.link.includes('amazon.com.br')) {
+                linkAfiliado = gerarLinkAfiliado(produto.link, AMAZON_AFFILIATE_TAG);
+            }
+
+            await postarNoCanal(produto, linkAfiliado, TELEGRAM_CHANNEL_ID);
+
+        
+            await new Promise(resolve => setTimeout(resolve, 5000));
         }
     } catch (error) {
-
-        console.error('ocorreu um erro no ciclo principal do bot:', error)
-
-    }finally {
-        console.log(' verificacao de ofertas finalizadas.')
-        console.log(' ====================================\n')
+        console.error('Ocorreu um erro no ciclo principal do bot:', error);
+    } finally {
+        console.log('Verificação de ofertas finalizada.');
+        console.log('====================================\n');
     }
 }
 
-cron.schedule('0 * * * *', () =>{
-    console.log('tarefa agenda sendo executada....')
-    rodarBot()
-})
+// 
+cron.schedule('*/30 * * * *', () => {
+    console.log('Tarefa agendada sendo executada...');
+    rodarBot();
+});
 
-console.log('Bot de promocoes inciado. primeira execucao em andamento...')
-rodarBot()
+console.log('Bot de promoções iniciado. Primeira execução em andamento...');
+rodarBot();
